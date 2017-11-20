ede4c974955a201c4def2c5755b3600f3443a7c2
lecas93 <angi_lecas@hotmail.com>
2017-11-20 02:05:30 -0600

    Update

--- src/Test/Logger.java
+++ src/Test/Logger.java
@@ -1,16 +1,84 @@
 package Test;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+//TODO
+//setLevel es decir, establecer los niveles
+//crear las funciones por cada nivel establecido
+
 public class Logger {
 
 	private static Logger log;
 
+	private static Logger log2;
+	private Class currentClass;
+	//public String id;
+
+	public static enum Level {
+		INFO, WARNING, SEVERE
+	}
+
+	private static int currentLevel = 0;
+
 	private Logger() {
 	}
 
+	/*
 	public static Logger getInstance() {
 		if (log == null) {
 			log = new Logger();
 		}
 		return log;
+	}
+	*/
+
+	// Con este metodo NO se debe aplicar el patron Singleton
+	public static Logger getLogger(Class myClass) {
+		Logger log = new Logger();
+		log.setCurrentClass(myClass);
+		//log.id = myClass.getName() + new Random().nextInt();
+		return log;
+	}
+
+	private void setCurrentClass(Class myClass) {
+		currentClass = myClass;
+	}
+
+	public void printId() {
+		//System.out.println(id);
+	}
+	
+	public void printLevel() {
+		System.out.println("Current level: " + currentLevel);
+	}
+
+	public static void setLevel(Level level) {
+		currentLevel = level.ordinal();
+		System.out.println("Current level: " + currentLevel);
+	}
+
+	private void printCurrentState() {
+		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+		System.out.println(
+				"" + new Date() + " " + currentClass.getSimpleName() + " " + stackTraceElements[3].getMethodName());
+	}
+
+	public void info(String msg) {
+		if (currentClass != null) {
+			printCurrentState();
+			System.out.println("INFO: " + msg);
+		}
+	}
+
+	public void warning(String msg) {
+		printCurrentState();
+		System.out.println("WARNING: " + msg);
+	}
+
+	public void severe(String msg) {
+		printCurrentState();
+		System.out.println("SEVERE: " + msg);
 	}
 }
--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,11 +1,61 @@
 package Test;
 
+import Test.Logger.Level;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Logger logger = Logger.getInstance();
-		// :)
+		//Logger logger = Logger.getInstance();
+		Logger.setLevel(Level.INFO);
+		
+		otroMetodo();
+		otroMetodo2();
+		yoloMetodo();
+		
+		Logger log2 = Logger.getLogger(Main.class);
+		log2.info("Hola");
+		
+		Logger log3 = Logger.getLogger(Main.class);
+		log3.info("Hola de nuevo");
+		
+		Logger log4 = Logger.getLogger(Main.class);
+		log4.warning("Este es un warning");
+		
+		//log4.printId();
+		//log3.printId();
+		//log2.printId();
+		
+		log4.printLevel();
+		log3.printLevel();
+		log2.printLevel();
+		
+		Logger.setLevel(Level.WARNING);
+		
+		log4.printLevel();
+		log3.printLevel();
+		log2.printLevel();
+		
+		Logger.setLevel(Level.SEVERE);
+		
+		log4.printLevel();
+		log3.printLevel();
+		log2.printLevel();
+	}
+	
+	public static void otroMetodo() {
+		Logger log2 = Logger.getLogger(Main.class);
+		log2.info("Hola");
+	}
+	
+	public static void otroMetodo2() {
+		Logger log2 = Logger.getLogger(Main.class);
+		log2.info("Hola de nuevo");
+	}
+	
+	public static void yoloMetodo() {
+		Logger log2 = Logger.getLogger(Main.class);
+		log2.warning("Este es un warning");
 	}
 
 }
